7781a9e9e6905d1ea085333bcd463d0da48a3f7f
wells5609 <wellspeterson@live.com>
2014-05-18 19:54:50 -0400

    Change to PSR-4

---
+++ src/Config.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * @package Phpf
+ */
+
+namespace Phpf\Config;
+
+use Phpf\Common\DataContainer;
+use RuntimeException;
+
+/**
+ * A basic Config object.
+ */
+class Config extends DataContainer
+{
+
+	protected $data;
+
+	protected $read_only = false;
+
+	protected $has_defaults = true;
+
+	protected $defaults = array();
+
+	public function __construct(array $data = array()) {
+		$this->data = $data;
+	}
+
+	/**
+	 * Sets a config item value.
+	 */
+	public function __set($var, $val) {
+
+		if ($this->isReadOnly()) {
+			throw new RuntimeException("Cannot set $var - config object is read-only.");
+		}
+
+		$this->data[$var] = $val;
+
+		return $this;
+	}
+
+	/**
+	 * Returns a config item value.
+	 */
+	public function __get($var) {
+
+		if (isset($this->data[$var])) {
+			return $this->data[$var];
+		}
+
+		return $this->hasDefaults() ? $this->getDefault($var) : null;
+	}
+
+	/**
+	 * Whether a config item exists.
+	 */
+	public function __isset($var) {
+
+		if (isset($this->data[$var])) {
+			return true;
+		}
+
+		return $this->hasDefaults() ? (bool)$this->getDefault($var) : false;
+	}
+
+	/**
+	 * Removes a config item.
+	 */
+	public function __unset($var) {
+
+		if ($this->isReadOnly()) {
+			throw new RuntimeException("Cannot unset $var - config object is read-only.");
+		}
+
+		unset($this->data[$var]);
+	}
+
+	/**
+	 * Sets a config item value and returns it.
+	 *
+	 * Useful for situations where you want to simulataneously set a config
+	 * property and another variable/object property/etc.
+	 */
+	public function setr($var, $val) {
+		$this->set($var, $val);
+		return $val;
+	}
+
+	/**
+	 * Set whether the config items are read-only.
+	 */
+	public function setReadOnly($val) {
+		$this->read_only = (bool)$val;
+		return $this;
+	}
+
+	/**
+	 * Set whether the config items can have defaults.
+	 */
+	public function setHasDefaults($val) {
+		$this->has_defaults = (bool)$val;
+		return $this;
+	}
+
+	/**
+	 * Whether the config items are read-only.
+	 */
+	public function isReadOnly() {
+		return $this->read_only;
+	}
+
+	/**
+	 * Whether the config items can have defaults.
+	 */
+	public function hasDefaults() {
+		return $this->has_defaults;
+	}
+
+	/**
+	 * Set an item's default value.
+	 */
+	public function setDefault($var, $val) {
+
+		if (! $this->hasDefaults()) {
+			throw new RuntimeException("Config object may not have defaults. To use defaults, call setHasDefaults(true)");
+		}
+
+		$this->defaults[$var] = $val;
+
+		return $this;
+	}
+
+	/**
+	 * Get an item's default value.
+	 */
+	public function getDefault($var) {
+
+		if (! $this->hasDefaults()) {
+			throw new RuntimeException("Config object does not have defaults.");
+		}
+
+		return isset($this->defaults[$var]) ? $this->defaults[$var] : null;
+	}
+
+}
--- src/File.php
+++ src/File.php
@@ -7,19 +7,21 @@
 namespace Phpf\Config;
 
 /**
- * Class for generating config object from a file. 
+ * Class for generating config object from a file.
  *
  * @package Config
  * @subpackage File
  */
-abstract class File extends Object {
-	
+abstract class File extends Config
+{
+
 	protected $configFile;
-	
+
 	abstract public function parse();
-	
-	public function setConfigFile( $file ){
+
+	public function setConfigFile($file) {
 		$this->configFile = $file;
 		return $this;
 	}
+
 }
